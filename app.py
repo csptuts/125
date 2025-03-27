b14849a838255e582ef7461eeb94f1aa7441b5f4
csptuts <85820742+csptuts@users.noreply.github.com>
2025-03-27 20:53:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,52 +6,58 @@ import files.sessionvars
 from files.wops import get_unixts
 from files.coredf import get_df25, get_df125
 from files.plotlyfig import chart_25, show_initial_chart
-from files.uploader import uploader
+
+
 
 with st.sidebar:
-    uploader()
+    uploaded_files = st.file_uploader("Choose CSV files", accept_multiple_files=True)
+    if uploaded_files is not None:
+        for uploaded_file in uploaded_files:
+            if uploaded_file.name == "weekly.csv":
+                st.session_state.dfW_0 = pd.read_csv(uploaded_file)
+            if uploaded_file.name == "125m.csv":
+                st.session_state.df125_0 = pd.read_csv(uploaded_file)
+            if uploaded_file.name == "25m.csv":
+                st.session_state.df25_0 = pd.read_csv(uploaded_file)
 
 with st.sidebar:
-    with st.form("my_form"):
-        st.write("Inside the form")
-        d = st.text_input("Enter date")
-        m = st.text_input("Enter month")
-        y = st.text_input("Enter Year")
+    with st.form("date_input"):
+        dmy = st.text_input("Enter date - in ddmmyy format")
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            weekly_df_subset=get_unixts(dmy)
+            st.session_state.weekly_df_subset = weekly_df_subset
+        
+
+with st.sidebar:
+    with st.form("Create Dropdown"):
+        # st.write("Inside the form")
+        dfy=st.session_state.weekly_df_subset
+        # st.dataframe(dfy)
+        selected_dt = st.selectbox('Select Week:', dfy[["TS-GMT"]])
+        # wk_unxts = st.text_input("Enter UNIXTS")
         
 
         # Every form must have a submit button.
         submitted = st.form_submit_button("Submit")
         if submitted:
-            st.write(d,m,y)
-            d=int(d)
-            m=int(m)
-            y=int(y)
-            sorted_df=get_unixts(d,m,y,dfW)
-            sorted_df1=sorted_df[["TS-GMT","UNIXTS"]]
-            st.dataframe(sorted_df1)
-
-
-
-
-
-with st.sidebar:
-    with st.form("my_form-1"):
-        st.write("Inside the form")
-        wk_unxts = st.text_input("Enter UNIXTS")
-        
-
-        # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            st.write(wk_unxts)
+            # st.dataframe(dfy)
+            dt_str=str(selected_dt)
+            dfx=st.session_state.weekly_df_subset.loc[st.session_state.weekly_df_subset['TS-GMT'] == dt_str]
+            last_row = dfx.iloc[-1]
+            wk_unxts=last_row.get(key = 'UNIXTS')
+            
+            # st.write(wk_unxts)
             wk_unxts=int(wk_unxts)
-            df25=get_df25(wk_unxts,df25_0)
-            df125=get_df125(wk_unxts,df125_0)
+            df25=get_df25(wk_unxts)
+            df125=get_df125(wk_unxts)
             plot_data=pd.merge_asof(df25,df125,left_index=True,right_index=True)
             plot_data['60rsi'] = 60
             plot_data['40rsi'] = 40
             unique_values = plot_data['IsoWeekNum'].unique()
-            st.write(unique_values)
+            # st.write(unique_values)
             wa=unique_values[0]
             wb=unique_values[1]
             grp=plot_data.groupby('IsoWeekNum')
@@ -65,7 +71,7 @@ with st.sidebar:
             else:
                 st.session_state.df1=grpa
                 st.session_state.df2=grpb
-            st.session_state.switch ='show'
+            # st.session_state.switch ='show'
             # st.session_state.df25=get_df25(wk_unxts,df25_0)
             # st.session_state.df125=get_df125(wk_unxts,df125_0)
             # sorted_df1=sorted_df[["TS-GMT","UNIXTS"]]
@@ -93,10 +99,10 @@ def greet():
         fig=chart_25(plot_df)
         st.plotly_chart(fig)
     else:
-       if st.button("Reset"):
-           st.session_state.row_num=0
-
-button("Forward", "ArrowRight", greet)
+        return
+        
+with st.sidebar:
+    button("Forward", "ArrowRight", greet)
 
 # if st.button("Forward" , ):
 #     # st.dataframe(st.session_state.df1)
@@ -117,11 +123,11 @@ button("Forward", "ArrowRight", greet)
 
 
 with st.container():
-    if st.session_state.switch =='show':
-        show_initial_chart()
-        st.session_state.switch ='hide'
-
-
-with st.sidebar:
-    if st.button("Reset"):
-       st.session_state.row_num=0 
+    show_initial_chart()
+    
+# with st.sidebar:
+#     if st.button("Reset"):
+#         st.session_state.row_num=0
+# with st.sidebar:
+#     if st.button("Reset"):
+#        st.session_state.row_num=0 
