479356f11349e97a36a14d4cbe46264d87e134c8
csptuts <85820742+csptuts@users.noreply.github.com>
2025-03-27 20:55:11 +0530

    Update coredf.py

--- files/coredf.py
+++ files/coredf.py
@@ -1,11 +1,15 @@
-def get_df25(wk_unxts,df25):
+import streamlit as st
+
+def get_df25(wk_unxts):
+  df25=st.session_state.df25_0
   plus=wk_unxts+604800
   minus=wk_unxts-604800
   df25 = df25[(df25['UNIXTS'] >= minus) & (df25['UNIXTS'] < plus)]
   df25.set_index('UNIXTS', inplace=True)
   return df25
 
-def get_df125(wk_unxts,df125):
+def get_df125(wk_unxts):
+  df125=st.session_state.df125_0
   plus=wk_unxts+604800
   minus=wk_unxts-604800
   df125 = df125[(df125['UNIXTS'] >= minus) & (df125['UNIXTS'] < plus)]
