5d6d2c238ff0789e6eeb9843ebd3266867fa559f
csptuts <85820742+csptuts@users.noreply.github.com>
2025-03-29 01:21:54 +0530

    Update wops.py

--- files/wops.py
+++ files/wops.py
@@ -3,15 +3,15 @@ import datetime
 import time
 import textwrap
 
-def get_unixts(dmy):
-    dfW = st.session_state.dfW_0
+def get_unixts(dmy,dfW_0):
+    # dfW = st.session_state.dfW_0
     lst=textwrap.wrap(dmy,2)
     d=int(lst[0])
     m=int(lst[1])
     y=int("20"+lst[2])
     date_time = datetime.datetime(y, m, d)
     ut=time.mktime(date_time.timetuple())
-    df_closest5 = dfW.iloc[(dfW['UNIXTS']-ut).abs().argsort()[:8]]
+    df_closest5 = dfW_0.iloc[(dfW_0['UNIXTS']-ut).abs().argsort()[:8]]
     sorted_df = df_closest5.sort_values(by=['UNIXTS'], ascending=True)
     # sorted_df=sorted_df[["TS-GMT","UNIXTS"]]
     # sorted_df['TS_diff'] = sorted_df['UNIXTS'].diff()
