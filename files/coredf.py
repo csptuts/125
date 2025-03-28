0616296209b592a623864fa046c93abc1347d4fe
csptuts <85820742+csptuts@users.noreply.github.com>
2025-03-29 01:21:05 +0530

    Update coredf.py

--- files/coredf.py
+++ files/coredf.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-def get_df25(wk_unxts):
-  df25=st.session_state.df25_0
+def get_df25(wk_unxts, df25_0):
+  # df25=st.session_state.df25_0
   plus=wk_unxts+604800
   minus=wk_unxts-604800
-  df25 = df25[(df25['UNIXTS'] >= minus) & (df25['UNIXTS'] < plus)]
+  df25 = df25_0[(df25_0['UNIXTS'] >= minus) & (df25_0['UNIXTS'] < plus)]
   df25.set_index('UNIXTS', inplace=True)
   return df25
 
-def get_df125(wk_unxts):
-  df125=st.session_state.df125_0
+def get_df125(wk_unxts, df125_0):
+  # df125=st.session_state.df125_0
   plus=wk_unxts+604800
   minus=wk_unxts-604800
-  df125 = df125[(df125['UNIXTS'] >= minus) & (df125['UNIXTS'] < plus)]
+  df125 = df125_0[(df125_0['UNIXTS'] >= minus) & (df125_0['UNIXTS'] < plus)]
   df125.set_index('UNIXTS', inplace=True)
   df125=df125[["RSI", "PP", "R1", "S1", "R2", "S2", ]]
   df125_cp = df125.copy()
