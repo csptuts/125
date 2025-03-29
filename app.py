6a3eec713855cf204ea3ecd6b2f0b3641244d1dc
csptuts <85820742+csptuts@users.noreply.github.com>
2025-03-29 19:31:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,145 @@
 import streamlit as st
 import pandas as pd
 from streamlit_shortcuts import button, add_keyboard_shortcuts
+import datetime
+import time
+import textwrap
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-import files.sessionvars
-from files.wops import get_unixts
-from files.coredf import get_df25, get_df125
-from files.plotlyfig import chart_25
+empty_df = pd.DataFrame(columns = ['TS-GMT'])
+
+if 'weekly_df_subset' not in st.session_state:
+    st.session_state.weekly_df_subset = empty_df
+
+if 'dfPrevWk' not in st.session_state:
+    st.session_state.dfPrevWk = 0
+
+if 'dfCurrentWk' not in st.session_state:
+    st.session_state.dfCurrentWk = 0    
+
+if 'row_num' not in st.session_state:
+    st.session_state.row_num = 0
+
+if 'total_rows' not in st.session_state:
+    st.session_state.total_rows = 0
+
+
+def get_unixts(dmy,dfW_0):
+    lst=textwrap.wrap(dmy,2)
+    d=int(lst[0])
+    m=int(lst[1])
+    y=int("20"+lst[2])
+    date_time = datetime.datetime(y, m, d)
+    ut=time.mktime(date_time.timetuple())
+    df_closest5 = dfW_0.iloc[(dfW_0['UNIXTS']-ut).abs().argsort()[:8]]
+    sorted_df = df_closest5.sort_values(by=['UNIXTS'], ascending=True)
+    return sorted_df
+    
+    
+def get_df25(wk_unxts, df25_0):
+  plus=wk_unxts+604800
+  minus=wk_unxts-604800
+  df25 = df25_0[(df25_0['UNIXTS'] >= minus) & (df25_0['UNIXTS'] < plus)]
+  df25.set_index('UNIXTS', inplace=True)
+  return df25
+
+def get_df125(wk_unxts, df125_0):
+  plus=wk_unxts+604800
+  minus=wk_unxts-604800
+  df125 = df125_0[(df125_0['UNIXTS'] >= minus) & (df125_0['UNIXTS'] < plus)]
+  df125.set_index('UNIXTS', inplace=True)
+  df125=df125[["RSI", "PP", "R1", "S1", "R2", "S2", ]]
+  df125_cp = df125.copy()
+  df125_cp['RSI'] = df125_cp['RSI'].shift(1)
+  df125=df125_cp.copy()
+  df125.dropna(inplace=True)
+  return df125
+    
+    
+    
+def chart_25(df):
+    
+  
+    fig = make_subplots(rows=2, cols=1, 
+                        shared_xaxes=True,
+                        vertical_spacing=0.01,
+                        row_heights=[1,0.1]
+    )
+
+ 
+    fig.add_trace(go.Candlestick(x=df['TS-GMT'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'], 
+                name="OHLC"),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=df['TS-GMT'],
+                             y=df['PP'],
+                             marker=dict(color='black',size=8),
+                             mode="markers",
+                             name="PP"),
+                            row=1, col=1)
+    
+    fig.add_trace(go.Scatter(x=df['TS-GMT'],
+                             y=df['R1'],
+                             marker=dict(color='orange',size=8),
+                             mode="markers",
+                             name="R1"),
+                             row=1, col=1)
+    
+    fig.add_trace(go.Scatter(x=df['TS-GMT'],
+                             y=df['R2'],
+                             marker=dict(color='orange',size=8),
+                             mode="markers",
+                             name="R2"),
+                             row=1, col=1)
+    
+    fig.add_trace(go.Scatter(x=df['TS-GMT'],
+                             y=df['S1'],
+                             marker=dict(color='green',size=8),
+                             mode="markers",
+                             name="S1"),
+                             row=1, col=1)
+    
+    fig.add_trace(go.Scatter(x=df['TS-GMT'],
+                             y=df['S2'],
+                             marker=dict(color='green',size=8),
+                             mode="markers",
+                             name="S2"),
+                             row=1, col=1)
+    
+    fig.add_trace(go.Scatter(x=df['TS-GMT'],
+                             y=df['SMA50'],
+                             marker=dict(color='blue',size=4),
+                             name="SMA50"),
+                             row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=df['TS-GMT'],
+                             y=df['RSI'],
+                             marker=dict(color='blue',size=4),
+                             mode="markers",
+                             name="RSI"),
+                             row=2, col=1)
+    
+    fig.add_trace(go.Scatter(x=df['TS-GMT'],
+                             y=df['60rsi'],
+                             line=dict(color='firebrick', width=1,dash='dash')),
+    row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=df['TS-GMT'],
+                             y=df['40rsi'], 
+                             line=dict(color='firebrick', width=1,dash='dash')),
+    row=2, col=1)
+    
+    fig.update_layout(xaxis_rangeslider_visible=False)
+    fig.update_layout(height=800, width=1200)
+    fig.update_yaxes(dtick=100)
+    fig.update_layout(showlegend=False)
+
+    return fig
 
 
 
@@ -33,23 +167,17 @@ with st.sidebar:
 
 with st.sidebar:
     with st.form("Create Dropdown"):
-        # st.write("Inside the form")
         dfy=st.session_state.weekly_df_subset
-        # st.dataframe(dfy)
         selected_dt = st.selectbox('Select Week:', dfy[["TS-GMT"]])
-        # wk_unxts = st.text_input("Enter UNIXTS")
         
-
         # Every form must have a submit button.
         submitted = st.form_submit_button("Submit")
         if submitted:
-            # st.dataframe(dfy)
             dt_str=str(selected_dt)
             dfx=st.session_state.weekly_df_subset.loc[st.session_state.weekly_df_subset['TS-GMT'] == dt_str]
             last_row = dfx.iloc[-1]
             wk_unxts=last_row.get(key = 'UNIXTS')
             
-            # st.write(wk_unxts)
             wk_unxts=int(wk_unxts)
             df25=get_df25(wk_unxts, df25_0)
             df125=get_df125(wk_unxts, df125_0)
@@ -57,7 +185,6 @@ with st.sidebar:
             plot_data['60rsi'] = 60
             plot_data['40rsi'] = 40
             unique_values = plot_data['IsoWeekNum'].unique()
-            # st.write(unique_values)
             wa=unique_values[0]
             wb=unique_values[1]
             grp=plot_data.groupby('IsoWeekNum')
@@ -71,43 +198,18 @@ with st.sidebar:
             else:
                 st.session_state.dfPrevWk=grpa
                 st.session_state.dfCurrentWk=grpb
-            # st.session_state.switch ='show'
-            # st.session_state.df25=get_df25(wk_unxts,df25_0)
-            # st.session_state.df125=get_df125(wk_unxts,df125_0)
-            # sorted_df1=sorted_df[["TS-GMT","UNIXTS"]]
-            # st.dataframe(sorted_df1)
-
-
-
-
+            
 def initialise():
-    # st.session_state.total_rows= len(st.session_state.df2)
-    # st.write(st.session_state.total_rows)
-    # st.session_state.row_num = st.session_state.row_num + 1
     if st.session_state.row_num < 1:
-        # sliced_df = st.session_state.df2.head(st.session_state.row_num)
-        # plot_df=pd.concat([st.session_state.df1,sliced_df], axis=0)
-        # last_row = sliced_df.iloc[-1]
-        # rsi=last_row.get(key = 'RSI') 
-        # st.write(rsi)
         fig=chart_25(st.session_state.dfPrevWk)
         st.plotly_chart(fig)
-    # else:
-    #     return
         
 with st.sidebar:
     button("Init", "Ctrl+ArrowRight", initialise)
 
 
-# if st.button("Initialise"):
-#     # st.dataframe(st.session_state.df1)
-#     # st.dataframe(st.session_state.df2)
-#     fig=chart_25(st.session_state.df1)
-#     st.plotly_chart(fig)
-
 def simulate():
     st.session_state.total_rows= len(st.session_state.dfCurrentWk)
-    # st.write(st.session_state.total_rows)
     st.session_state.row_num = st.session_state.row_num + 1
     if st.session_state.row_num <= st.session_state.total_rows:
         sliced_df = st.session_state.dfCurrentWk.head(st.session_state.row_num)
@@ -117,29 +219,9 @@ def simulate():
         st.write(rsi)
         fig=chart_25(plot_df)
         st.plotly_chart(fig)
-    # else:
-    #     return
         
 with st.sidebar:
     button("Forward", "ArrowRight", simulate)
-
-# if st.button("Forward" , ):
-#     # st.dataframe(st.session_state.df1)
-#     # st.dataframe(st.session_state.df2)
-#     # fig=chart_25(st.session_state.df1)
-#     # st.plotly_chart(fig)
-#     st.write('Hello')
-#     st.session_state.total_rows= len(st.session_state.df2)
-#     st.write(st.session_state.total_rows)
-#     st.session_state.row_num = st.session_state.row_num + 1
-#     if st.session_state.row_num <= st.session_state.total_rows:
-#         sliced_df = st.session_state.df2.head(st.session_state.row_num)
-#         plot_df=pd.concat([st.session_state.df1,sliced_df], axis=0)
-#         fig=chart_25(plot_df)
-#         st.plotly_chart(fig)
-#     else:
-#         st.stop()
-
 
 
 with st.sidebar:
@@ -170,4 +252,3 @@ with st.sidebar:
             st.session_state.total_rows = 0
         else:
             st.session_state.total_rows = 0
-        
